6e768fe2f68bb9f1dc8322be39d59340e3ca8d1e
Dan Ditomaso <dan.ditomaso@gmail.com>
2024-10-08 17:34:23 -0400

    feat: removed duplicate analytics component

--- app/components/analytics/tracking.tsx
+++ app/components/analytics/tracking.tsx
@@ -1,16 +1,8 @@
-"use client";
-
 export function Analytics() {
-	const token = process.env.NEXT_PUBLIC_BEAM_TOKEN;
+  const token = process.env.NEXT_PUBLIC_BEAM_TOKEN;
 
-	if (!token) {
-		return null;
-	}
-	return (
-		<script
-			src="https://beamanalytics.b-cdn.net/beam.min.js"
-			data-token={token}
-			async
-		/>
-	);
+  if (!token) {
+    return null;
+  }
+  return <script src="/vendor/analytics/beam.min.js" data-token={token} async />;
 }
