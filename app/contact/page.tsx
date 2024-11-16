952d801b6f95820b186236311a952b6285b0f272
Dan Ditomaso <dan.ditomaso@gmail.com>
2024-11-15 22:52:50 -0500

    fix: cant have dup named expot in page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,27 +1,5 @@
-import { FaEnvelope, FaGithub, FaTwitter } from "react-icons/fa";
 import { Navigation } from "../components/nav";
 import { Socials } from "../components/socials";
-
-export const socials = [
-  {
-    icon: <FaTwitter size={20} />,
-    href: "https://twitter.com/danditomaso",
-    label: "Twitter",
-    handle: "@danditomaso",
-  },
-  {
-    icon: <FaEnvelope size={20} />,
-    href: "mailto:hi@danditomaso.com?subject=Inquiry From Website",
-    label: "Email",
-    handle: "hi@danditomaso.com",
-  },
-  {
-    icon: <FaGithub size={20} />,
-    href: "https://github.com/danditomaso",
-    label: "Github",
-    handle: "danditomaso",
-  },
-];
 
 export default async function ContactPage() {
   return (
